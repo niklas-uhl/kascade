114d610b094ff345691f8e0247dc7f322809af06
Niklas Uhl <uhl@kit.edu>
2026-02-06 12:30:12 +0100

    exp: Map additional graph parameters.

--- eval/KascadeEval.jl
+++ eval/KascadeEval.jl
@@ -14,7 +14,8 @@ function cleanup(df)
         return df
     end
     transform!(df, :time => ByRow(expand_timers) => AsTable)
-    allowed_kagen_args = Set(["type", "n", "m", "permute"])
+    allowed_kagen_args = Set(["type", "n", "m", "p", "permute", "gamma"])
+    args_remap = Dict("p" => "prob")
     function expand_graph(kagen_option_string::String)
         function parse_value(val)
             try
@@ -29,7 +30,7 @@ function cleanup(df)
         end
         pairs = split(kagen_option_string, ';')
         result = Dict{Symbol, Any}()
-        result[:permute] = false # TODO: remove this 
+        result[:permute] = false # TODO: remove this
         for pair in pairs
             # treat as bool if pair contains no '='
             if !occursin('=', pair)
@@ -39,10 +40,16 @@ function cleanup(df)
                 key, val = split(pair, '=')
             end
             if key in allowed_kagen_args
+                if haskey(args_remap, key)
+                    key = args_remap[key]
+                end
                 result[Symbol(key)] = parse_value(val)
             end
         end
         for key in allowed_kagen_args
+            if haskey(args_remap, key)
+                key = args_remap[key]
+            end
             if Symbol(key) ∉ keys(result)
                 result[Symbol(key)] = missing
             end
@@ -55,7 +62,10 @@ function cleanup(df)
     replace!(df.type, "gnm-undirected" => "gnm")
     @subset!(df, :iteration .>= Config.first_iteration)
     sort!(df, :p)
-    function to_graph_string(type, n, m)
+    function to_graph_string(;kwargs...)
+        type = kwargs[:type]
+        n = kwargs[:n]
+        m = kwargs[:m]
         result = "$type("
         if n !== missing
             result *= "$(Int(log2(n)))"
@@ -63,10 +73,19 @@ function cleanup(df)
         if m !== missing
             result *= ",$(Int(log2(m)))"
         end
+        if kwargs[:permute] == true
+            result *= ",permute"
+        end
+        if kwargs[:gamma] !== missing
+            result *= ",gamma=$(kwargs[:gamma])"
+        end
+        if kwargs[:prob] !== missing
+            result *= ",prob=$(kwargs[:prob])"
+        end
         result *= ")"
         return result
     end
-    transform!(df, [:type, :n, :m] => ByRow(to_graph_string) => :graph)
+    transform!(df, AsTable(:) => ByRow(t -> to_graph_string(;t...)) => :graph)
     return select!(df, Not([:kagen_option_string, :time]))
 end
 
