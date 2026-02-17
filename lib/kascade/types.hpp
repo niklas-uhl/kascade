04bdb049f02bdc945843d092a28d2d02cbcbc29b
Niklas Uhl <uhl@kit.edu>
2026-02-17 08:28:57 +0100

    feat(types): Add IndexRange concept for forward ranges of idx_t
    - Introduce IndexRange concept requiring forward_range of idx_t
    - Include <ranges> and tidy namespace closing comments

--- lib/kascade/types.hpp
+++ lib/kascade/types.hpp
@@ -2,8 +2,13 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <ranges>
 
 namespace kascade {
-  using idx_t = std::size_t;
-  using rank_t = std::int64_t;
-}
+using idx_t = std::size_t;
+using rank_t = std::int64_t;
+
+template <typename R>
+concept IndexRange =
+    std::ranges::forward_range<R> && std::same_as<std::ranges::range_value_t<R>, idx_t>;
+}  // namespace kascade
