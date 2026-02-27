ac504378d3b8ea52e934f38e9f0439ed74b6198d
Niklas Uhl <uhl@kit.edu>
2026-02-27 11:28:39 +0100

    feat(sparse_ruling_set): Add format_as for NodeType.

--- lib/kascade/sparse_ruling_set_detail/types.hpp
+++ lib/kascade/sparse_ruling_set_detail/types.hpp
@@ -1,12 +1,30 @@
 #pragma once
 
 #include <cstdint>
+#include <string_view>
 
 #include "kascade/types.hpp"
 
 namespace kascade {
 
 enum class NodeType : std::uint8_t { root, leaf, ruler, unreached, reached };
+
+inline auto format_as(NodeType type) -> std::string_view {
+  switch (type) {
+    case NodeType::root:
+      return "root";
+    case NodeType::leaf:
+      return "leaf";
+    case NodeType::ruler:
+      return "ruler";
+    case NodeType::unreached:
+      return "unreached";
+    case NodeType::reached:
+      return "reached";
+      break;
+  }
+  std::unreachable();
+}
 
 struct RulerMessage {
   idx_t target_idx;
